424bc8d6704699c87a473644a0b31b2d1ffddde3
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-07-14 14:37:11 -0500

    Remove extra config options

--- app/app.py
+++ app/app.py
@@ -4,30 +4,24 @@ from typing import Mapping
 from flask import Flask, render_template, Response
 
 
-def createApp(test_config: Mapping = None):
+def createApp():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
-    if test_config is None:
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        app.config.from_mapping(test_config)
-
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
     @app.route("/submit")
     def hello():
         return Response("hello", mimetype="text/plain")
 
     @app.route("/cwd")
-    def test():
+    def cwd():
         return Response(os.getcwd(), mimetype="text/plain")
+
+    @app.route("/life")
+    def life():
+        return Response("\n".join(os.listdir(".")), mimetype="text/plain")
 
     @app.route("/")
     def root():
