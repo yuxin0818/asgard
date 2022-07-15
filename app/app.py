ac073768240c57aa229bb9494fabe32620d85469
acgrigores <acgrigores@gmail.com>
2022-07-15 15:30:17 -0500

    Uploaded files

--- app/app.py
+++ app/app.py
@@ -32,21 +32,32 @@ def createApp():
 
     @app.route("/", methods=["GET", "POST"])
     def home():
-        if request.method == "POST":
-            if "file" not in request.files:
-                flash("No file part")
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
                 return redirect(request.url)
-            file = request.files["file"]
+            file = request.files['file']
+            print(type(request))
             # If the user does not select a file, the browser submits an
             # empty file without a filename.
-            if file.filename == "":
-                flash("No selected file")
+            if file.filename == '':
+                flash('No selected file')
                 return redirect(request.url)
             if file:
                 filename = secure_filename(file.filename)
-                file.save(filename)
-                return redirect(url_for("download_file", name=filename))
-            return render_template("index.html")
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect(url_for('upload_file', name=filename))
+        # return '''
+        # <!doctype html>
+        # <title>Upload new File</title>
+        # <h1>Upload new File</h1>
+        # <form method=post enctype=multipart/form-data>
+        # <input type=file name=file>
+        # <input type=submit value=Upload>
+        # </form>
+        # '''
+        return render_template("index.html")
 
     @app.route("/uploader", methods=["GET", "POST"])
     def upload_file():
