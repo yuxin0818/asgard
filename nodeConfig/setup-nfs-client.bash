2a15be68ffad2988f57d243b2be90c8f84502d92
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-07-18 12:09:28 -0500

    Renamed file

---
+++ nodeConfig/setup-nfs-client.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Tutorial followed
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install nfs-common
+sudo rm -r /mnt/documents_node1
+sudo mkdir -p /mnt/documents_node1
+sudo mount 10.22.75.78:/mnt/documents /mnt/documents_node1
