5cda2aa067fbb9e53972bdeacc487791649c43a2
@rsethi <70125166+rsethi21@users.noreply.github.com>
2022-07-26 13:08:26 -0500

    Update setup-avahi.bash

--- nodeConfig/setup-avahi.bash
+++ nodeConfig/setup-avahi.bash
@@ -3,4 +3,4 @@ sudo apt upgrade -y
 sudo apt autoremove --purge -y
 sudo apt-get install -y avahi-daemon
 sudo systemctl enable avahi-daemon.server
-sed -i 's/hosts*/hosts: mymachines mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] files myhostname dns/' /etc/nsswitch.conf
+sudo systemctl start avahi-daemon.server
