1e8697c17d5a22d2ab828d110af55571718e7d21
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-07-27 13:18:54 -0500

    Clean up the code

--- serverConfig/setupNFS.bash
+++ serverConfig/setupNFS.bash
@@ -3,16 +3,15 @@
 # Tutorials followed:
 # https://phoenixnap.com/kb/ubuntu-nfs-server
 
-# Variables
 folderPath="/mnt/asgard"
-subnetIP=$1
+subnetIP="192.168.102.0"
 
-# Update, Upgrade and Autoremove software
+sudo apt remove nfs-kernel-server --purge -y
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove --purge -y
 
-# Install software
 sudo apt install nfs-kernel-server -y
 
 # Setup shared folder
@@ -21,16 +20,15 @@ sudo mkdir $folderPath
 sudo chown nobody:nogroup $folderPath
 sudo chmod 777 $folderPath
 
-# Add IP addresses for devices that can connect to the HEAD node
-sudo cp /etc/exports /etc/exports.backup2
+# Expose shared folder to the subnet
+sudo cp /etc/exports /etc/exports.backupd
 echo "${folderPath} ${subnetIP}/24(rw,sync,no_subtree_check)" | sudo tee /etc/exports
 
-# Export file system
 sudo exportfs -a
 
-# Restart NFS server
 sudo systemctl restart nfs-kernel-server
+sudo systemctl status nfs-kernel-server
 
 # Add subnet IP addresses to pass through the firewall
-sudo ufw allow from 10.22.74.229 to any port nfs
+sudo ufw allow from "${subnetIP}/24" to any port nfs
 sudo ufw status
