cfa9fb6b66c62d41236fc38a55d3c9bf3d962ae8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-10-04 13:36:05 -0500

    Remove comments

--- app/server.py
+++ app/server.py
@@ -20,32 +20,20 @@ def createApp():
 
     @app.route("/", methods=["GET", "POST"])
     def home():
-        if request.method == 'POST':
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                flash('No file part')
+        if request.method == "POST":
+            if "file" not in request.files:
+                flash("No file part")
                 return redirect(request.url)
-            file = request.files['file']
+            file = request.files["file"]
             print(type(request))
-            # If the user does not select a file, the browser submits an
-            # empty file without a filename.
-            if file.filename == '':
-                flash('No selected file')
+            if file.filename == "":
+                flash("No selected file")
                 return redirect(request.url)
             if file:
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
                 flash("File Uploaded Successfully")
-                # return redirect(url_for('upload_file'))
         return render_template("index.html")
-
-    # @app.route("/upload", methods=["GET", "POST"])
-    # def upload_file():
-    #     # if request.method == "POST":
-    #     #     f = request.files["file"]
-    #     #     f.save(secure_filename(f.filename))
-    #     #     return "File uploaded successfully"
-    #     return render_template("index.html")
 
     return app
 
